ad6f22267a8ccb8a2ee7e9fd6da3fc943fb80ebd
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-07-18 16:28:33 +0300

    Update and rename ModonInitialPortalJS12.js to ModonInitialPortalJS13.js

--- ModonInitialPortalJS13.js
+++ ModonInitialPortalJS13.js
@@ -1,21 +1,10 @@
-var baseURL;
-var infoIconURL = "https://cdn.jsdelivr.net/gh/julianogharzeddine/ModonImages@main/InfoIcon.png"
-var currentLanguage;
-var dictionary;
+var baseURL;   // fetching the base URL
+var infoIconURL = "https://srv-k2five/Designer/Runtime/File.ashx?_path=gj%2FkK8xKydFGIaBuABcSOduRX2c%3D%5Cinformation-button.png&_filerequestdata=_4XeJqbaWuJp0syOgJHEA8of-kLRzEyrnXA94YOkjasqBnsOTQgJXJ-Ybt28RDf8-rNsOJTV6GFCRJMwfDiB-T1qyY65WRx0Csth2wTf9JOReKkiiOYspbS7vEwYNJBIywx1kBd-LFpHYtIPS0xUdrkixdScIEVBKIgcyqXW3WD2a1CNZt1TjOmkHTF0prdAe6Kyil_9PHynI0KFBGfxlSpFuMC_LFnUMkZaxgfFrVy1zuKMnYwsZLNdUnn1Fg3F02l-Z5JDdXl-ChygqFDt0QT0TpYjnxCCkjbfYOS8_pU1&_height=32&_width=32&_controltype=image&XSRFToken=4399624675727584330"
 
 $(document).ready(function () {
 
-
-    // Defining the dictionary 
-
-    dictionary = [
-        { "English": "Our Departments", "Arabic": "أقسامنا المختلفة", "French": "Nos Services" },
-    ];
-
-    // Setting the current language
-    currentLanguage = getLanguage();
-
     // Fetching the baseURL to use it in subsequent API Calls
+
     baseURL = window.location.protocol + '//' + window.location.host + '/';
 
 
@@ -30,11 +19,7 @@ $(document).ready(function () {
             console.error(error);
         });
 
-
-    $(document).on('click', ".dd-container", function () {
-        changeLanguage()
-    })
-
+    createModal();
 
     $(document).on('click', ".dd-container a", function () {
         changeLanguage()
@@ -69,78 +54,93 @@ $(document).ready(function () {
         $("#serviceLink").val("")
     })
 
-    waitForTranslatorHeaderRender()
 
-})
-
-function waitForWrapperRender(data) {
-    if ($('#sectionBrowser').length > 0) {
-        renderTiles(data);
-    } else {
-        setTimeout(waitForWrapperRender, 500);
+    function waitForWrapperRender(data) {
+        if ($('#sectionBrowser').length > 0) {
+            renderTiles(data);
+        } else {
+            setTimeout(waitForWrapperRender, 500);
+        }
     }
-}
 
-function waitForTranslatorHeaderRender() {
-    if ($('.dd-select').length > 0) {
-        $('.dd-select').click()
-        $('.dd-select').click()
-    } else {
-        setTimeout(waitForTranslatorHeaderRender, 50);
-    }
-}
+    function fetchTiles() {
 
-function fetchTiles() {
-
-    return new Promise(function (resolve, reject) {
-        $.ajax({
-            type: 'GET',
-            url: `${baseURL}api/odatav4/v4/ModonSections`,
-            dataType: 'json',
-            crossDomain: false,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent("sp_admin" + ':' + "P@ssw0rd"))));
-                xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-                xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-            },
-            success: function (json_data) {
-                resolve(json_data.value);
-            },
-            error: function () {
-                reject('Failed to Load Service Tiles!');
-            }
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                type: 'GET',
+                url: `${baseURL}api/odatav4/v4/sections_SMO`,
+                dataType: 'json',
+                crossDomain: false,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent("sp_admin" + ':' + "P@ssw0rd"))));
+                    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+                    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+                },
+                success: function (json_data) {
+                    resolve(json_data.value);
+                },
+                error: function () {
+                    reject('Failed to Load Service Tiles!');
+                }
+            });
         });
-    });
-}
+    }
 
-function renderTiles(data) {
+    function renderTiles(data) {
 
-    $('#sectionBrowser').html('')
-    $('#sectionBrowser').append('<p class="sectionBrowserTitle" id="OurDepartments" class="translatable">أقسامنا المختلفة</p>')
-    $("#sectionBrowser").append("<div id='card-wrapper'></div>")
+        $('#sectionBrowser').html('')
+        $('#sectionBrowser').append('<p class="sectionBrowserTitle" id="OurDepartments">أقسامنا المختلفة</p>')
+        $("#sectionBrowser").append("<div id='card-wrapper'></div>")
 
-    data.map((tile) => {
-        if (tile.IsActive == 'true') {
+        data.map((tile) => {
             $("#card-wrapper").append(`
-          <div class="cardItem"  id="${tile.JavaScriptID}" onclick="goTo('${tile.ServiceURL ?? ""}')">
+          <div class="cardItem" onclick="goTo('${tile.ServiceURL ?? ""}')">
           <div class="infoIconContainer">
           <img src="${infoIconURL}"
             class='infoIcon'>
           </div>
           <img src="${tile.ServiceImage}" class='titleImage'>
-          <p class="cardTitle">${currentLanguage == 'ar-SA' ? tile.ServiceNameAR : tile.ServiceNameEN}</p>
+          <p class="cardTitle" id='LegalAffairs'>${tile.ServiceNameAR}</p>
           </div>
         `)
+        })
+    }
+
+    function goTo(href) {
+        if (href) {
+            window.open(href, "_self")
         }
 
-    })
-}
-
-function goTo(href) {
-    if (href) {
-        window.open(href, "_self")
     }
-}
+
+
+
+    // Translating the Page On Load
+
+    setTimeout(function () {
+        let LSLang = localStorage.getItem('selected_language')
+
+        switch (LSLang) {
+            case 'en-US':
+                $("a.dd-option label.dd-option-text:contains('Arabic')").click();
+                $("a.dd-option label.dd-option-text:contains('English')").click();
+                break
+            case 'ar-SA':
+                $("a.dd-option label.dd-option-text:contains('Arabic')").click();
+                $("a.dd-option label.dd-option-text:contains('Arabic')").click();
+                break
+            case 'fr-FR':
+                $("a.dd-option label.dd-option-text:contains('Arabic')").click();
+                $("a.dd-option label.dd-option-text:contains('Français')").click();
+                break
+            default:
+                $("a.dd-option label.dd-option-text:contains('Arabic')").click();
+
+                break
+        }
+    }, 2000)
+
+})
 
 function renderNewService(serviceName) {
     $('#card-wrapper').append(
@@ -155,34 +155,22 @@ function renderNewService(serviceName) {
 
 }
 
-function changeLanguage() {
-    
- var targetLang ;
-    setTimeout(function () {
 
-        if (currentLanguage == "en-US") {
-            targetLang = "English"
+function changeLanguage() {
+
+    setTimeout(function () {
+        var lang = localStorage.getItem("selected_language")
+
+        if (lang == "en-US") {
             translateToEnglish()
-        } else if (currentLanguage == 'ar-SA') {
-            targetLang = "Arabic"
+        } else if (lang == 'ar-SA') {
             translateToArabic()
         }
-
-        translateText(targetLang)
-
-
     }, 500)
 
 }
 
-function translateText(lang) {
 
-    let toTranslate = $('.translatable')
-
-    toTranslate.each(function () {
-        $(this).text(getFromDictionary(($(this).text().trim()), lang))
-    })
-}
 
 
 function translateToEnglish() {
@@ -245,34 +233,4 @@ function createModal() {
 </div>
 </div>
 `)
-}
-
-function getLanguage() {
-    return localStorage.getItem('selected_language')
-}
-
-function getFromDictionary(text, toLanguage) {
-    for (var i = 0; i < dictionary.length; i++) {
-
-        var entry = dictionary[i];
-
-        if (entry.English === text) return entry[toLanguage];
-        if (entry.Arabic === text) return entry[toLanguage];
-        if (entry.French === text) return entry[toLanguage];
-
-    }
-
-    return 'Translation not found';
-}
-
-function initiateTiles() {
-
-    fetchTiles()
-        .then(function (data) {
-            // Wait for the card-wrapper div to render successfully
-            waitForWrapperRender(data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
 }
