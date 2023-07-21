05acb5164e3ff0306b2f32b285db6f915c72d434
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-07-21 11:14:18 +0300

    Update and rename FormJS1.js to FormJS2.js

---
+++ FormJS2.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function () {
+
+    const currentURL = window.location.href;
+    const searchKeyword = "RuntimeAR";
+
+    if (currentURL.includes(searchKeyword)) {
+        $(`div[name*=Card] .panel-header-wrapper .panel-header-text,div[name*=Card] .grid-header-wrapper .grid-header-text`).css('text-align', 'right')
+    } else {
+        $(`div[name*=Card] .panel-header-wrapper .panel-header-text,div[name*=Card] .grid-header-wrapper .grid-header-text`).css('text-align', 'left')
+    }
+
+    $("[name='EnglishFlag']").on("click", function () {
+        updateURL("EnglishFlag");
+    });
+
+    $("[name='ArabicFlag']").on("click", function () {
+        updateURL("ArabicFlag");
+    });
+
+});
+
+// Function to update the URL based on flag clicks
+function updateURL(keyword) {
+    const currentURL = window.location.href;
+    if (keyword === "EnglishFlag") {
+        window.history.replaceState(null, "", currentURL.replace("RuntimeAR", "Runtime"));
+    } else if (keyword === "ArabicFlag") {
+        window.history.replaceState(null, "", currentURL.replace("Runtime", "RuntimeAR"));
+    }
+}
+
+function changeLang() {
+    var lang = localStorage.getItem("selected_language")
+    if (lang == "ar-SA") {
+        $(".row:has(div[name*=Card])").css("flex-direction", "row-reverse");
+        $('.file-info-cell').find('div.file-watermark').text("إضغط هنا لتحميل ملف")
+    } else {
+        $(".row:has(div[name*=Card])").css("flex-direction", "row");
+        $('.file-info-cell').find('div.file-watermark').text("Click here to attach a file")
+    }
+}
+function waitForWrapperRenderForLang() {
+    var s = $('.dd-container'); if (s.length > 0) {
+        $(".dd-container a").on("click", () => changeLang())
+        setTimeout(() => {
+            $(".dd-selected").trigger("click")
+            $(".dd-selected").trigger("click")
+        }, 500);
+    } else { setTimeout(waitForWrapperRenderForLang, 300); }
+}
