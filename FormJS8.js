5ebcec1df2a28ca4464fcbd3fa76804b57be42a2
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-07-25 15:02:29 +0300

    Update and rename FormJS7.js to FormJS8.js

--- FormJS8.js
+++ FormJS8.js
@@ -11,13 +11,13 @@ $(document).ready(function () {
 
        $(`div[name*=Card] .panel-header-wrapper .panel-header-text,div[name*=Card] .grid-header-wrapper .grid-header-text`).css('text-align', 'right')
        $('.reToolbarWrapper').css("display" , "flex")
-       $('.reToolbarWrapper').css("flex-direction" , "row-reverse")
+
     } else {
 
         $(`div[name*=Card] .panel-header-wrapper .panel-header-text,div[name*=Card] .grid-header-wrapper .grid-header-text`).css('text-align', 'left')
         $('.reToolbarWrapper').css("display" , "flex")
-        $('.reToolbarWrapper').css("flex-direction" , "row")
     }
+    $('.reToolbarWrapper').css("flex-direction" , "row")
 
     $("[name='EnglishFlag']").on("click", function () {
         updateURL("EnglishFlag");
