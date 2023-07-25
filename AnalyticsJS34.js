681835bb1ec113de36ed75e6a1323fd8a2cb02e9
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-07-25 12:57:10 +0300

    Update and rename AnalyticsJS33.js to AnalyticsJS34.js

--- AnalyticsJS34.js
+++ AnalyticsJS34.js
@@ -9,10 +9,58 @@ $(document).ready(function () {
 
     }, 3000)
 
+    $(document).on('click', ".dd-container a", function () {
+        changeLanguage()
+    })
+
+    $(document).on('click', ".dd-container", function () {
+        changeLanguage()
+    })
+
+    waitForWrapperRenderForLang()
+
 })
 
-function initializeDrawChart() {
+function translateToEnglish() {
+  
+    $("[name='Sidebar']").css('right', '')
+    $("[name='Sidebar']").css('left', '0')
+    $(".form").css('right', '')
+    $(".form").css('left', '22%')
+    $('.taskDD').css('left', '76%')
+    $('.taskDD a').css('flex-direction', 'row')
+    $('.task-details p').css({
+        'text-align': 'left',
+        'direction': 'rtl'
+    })
+    $(".task-details h4").css("text-align", "left")
+}
 
+function translateToArabic() {
+    $("[name='Sidebar']").css('left', '')
+    $("[name='Sidebar']").css('right', '0')
+    $(".form").css('left', '')
+    $(".form").css('right', '21%')
+    $('.taskDD').css('left', '19%')
+    $('.taskDD a').css('flex-direction', 'row-reverse')
+    $('.task-details p').css({
+        'text-align': 'right',
+        'direction': 'ltr'
+    })
+    $(".task-details h4").css("text-align", "right")
+}
+
+
+function waitForWrapperRenderForLang() {
+    var s = $('.dd-container');
+    if (s.length > 0) {
+        $('.dd-container').trigger("click")
+    } else {
+        setTimeout(waitForWrapperRenderForLang, 300);
+    }
+}
+
+function initializeDrawChart() {
 
     fetchVacancies()
         .then(function (data) {
@@ -23,8 +71,6 @@ function initializeDrawChart() {
         });
 
 }
-
-// Wait for the Card Wrapper
 
 function waitForReportsWrapperRender(data) {
     if ($('#vacancy-reports').length > 0) {
@@ -41,8 +87,6 @@ function renderReports(data) {
     drawVacanciesByJobTitleChart(data)
     drawVacanciesByYearsOfExperienceChart(data)
 }
-
-
 
 function drawVacancyByDepartmentChart(data) {
     // Parse the JSON response (replace this with your actual JSON response)
@@ -83,7 +127,6 @@ function drawVacancyByDepartmentChart(data) {
     chart.draw(dataTable, options);
 }
 
-
 function drawVacanciesByJobTitleChart(data) {
     var jsonResponse = data
 
@@ -93,21 +136,21 @@ function drawVacanciesByJobTitleChart(data) {
 
     var jobTitleCount = {};
     jsonResponse.value.forEach(vacancy => {
-      var jobTitle = vacancy.JobTitle;
-      jobTitleCount[jobTitle] = jobTitleCount[jobTitle] ? jobTitleCount[jobTitle] + 1 : 1;
+        var jobTitle = vacancy.JobTitle;
+        jobTitleCount[jobTitle] = jobTitleCount[jobTitle] ? jobTitleCount[jobTitle] + 1 : 1;
     });
 
     Object.keys(jobTitleCount).forEach(jobTitle => {
-      data.addRow([jobTitle, jobTitleCount[jobTitle]]);
+        data.addRow([jobTitle, jobTitleCount[jobTitle]]);
     });
 
     var options = {
-      chartArea: { width: '50%' },
-      hAxis: { title: 'Number of Vacancies', minValue: 0 },
-      vAxis: { title: 'Job Title' }
+        chartArea: { width: '50%' },
+        hAxis: { title: 'Number of Vacancies', minValue: 0 },
+        vAxis: { title: 'Job Title' }
     };
 
-  // Add the vacanciesByJobTitle chart container
+    // Add the vacanciesByJobTitle chart container
     $('#vacancy-reports').append(`<div class='report-wrapper'>
     <p class='reportTitle'> Vacancies By Job Title </p>
     <div id="vacanciesByJobTitle"></div>
@@ -115,14 +158,10 @@ function drawVacanciesByJobTitleChart(data) {
 
     var chart = new google.visualization.BarChart(document.getElementById('vacanciesByJobTitle'));
     chart.draw(data, options);
-  
-
-
-  }
 
 
 
-
+}
 
 function drawVacanciesByQualTypeChart(data) {
     var jsonResponse = data;
@@ -168,41 +207,40 @@ function drawVacanciesByQualTypeChart(data) {
 
 function drawVacancyStatusChart(data) {
     var jsonResponse = data;
-  
+
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Status');
     data.addColumn('number', 'Count');
-  
+
     var statusCount = {};
     jsonResponse.value.forEach(vacancy => {
-      var status = vacancy.Status;
-      statusCount[status] = statusCount[status] ? statusCount[status] + 1 : 1;
+        var status = vacancy.Status;
+        statusCount[status] = statusCount[status] ? statusCount[status] + 1 : 1;
     });
-  
+
     Object.keys(statusCount).forEach(status => {
-      data.addRow([status, statusCount[status]]);
+        data.addRow([status, statusCount[status]]);
     });
-  
+
     var options = {
-      chartArea: { width: '90%', height: '90%' }, // Increase the chart area to make the whole chart larger
-      colors: ['#4285F4', '#DB4437', '#F4B400', '#0F9D58', '#AB47BC'], // Change the colors as needed
-      pieHole: 0.4, // Make a donut chart by setting the pieHole option (0.4 creates a small hole in the middle)
-      pieSliceText: 'percentage', // Show the percentage of each slice on the chart
-      legend: { position: 'right', textStyle: { fontSize: 14 } } // Position the legend on the right side of the chart and increase its font size
+        chartArea: { width: '90%', height: '90%' }, // Increase the chart area to make the whole chart larger
+        colors: ['#4285F4', '#DB4437', '#F4B400', '#0F9D58', '#AB47BC'], // Change the colors as needed
+        pieHole: 0.4, // Make a donut chart by setting the pieHole option (0.4 creates a small hole in the middle)
+        pieSliceText: 'percentage', // Show the percentage of each slice on the chart
+        legend: { position: 'right', textStyle: { fontSize: 14 } } // Position the legend on the right side of the chart and increase its font size
     };
-  
+
     // Add the vacanciesStatus chart container
     $('#vacancy-reports-doublecardwrapper').append(`<div class='report-wrapper'>
       <p class='reportTitle'> Vacancy Status </p>
       <div id="vacanciesStatus"></div>
       </div>`);
-  
+
     var chart = new google.visualization.PieChart(document.getElementById('vacanciesStatus'));
     chart.draw(data, options);
-  }
-  
+}
 
-  function drawVacanciesByYearsOfExperienceChart(data) {
+function drawVacanciesByYearsOfExperienceChart(data) {
     var jsonResponse = data
 
     var data = new google.visualization.DataTable();
@@ -213,44 +251,44 @@ function drawVacancyStatusChart(data) {
     var totalVacancies = jsonResponse.value.length;
 
     jsonResponse.value.forEach(vacancy => {
-      var expYears = vacancy.ExpertiseYears;
-      expYearsCount[expYears] = expYearsCount[expYears] ? expYearsCount[expYears] + 1 : 1;
+        var expYears = vacancy.ExpertiseYears;
+        expYearsCount[expYears] = expYearsCount[expYears] ? expYearsCount[expYears] + 1 : 1;
     });
 
     Object.keys(expYearsCount).forEach(expYears => {
-      var percentage = (expYearsCount[expYears] / totalVacancies) * 100;
-      data.addRow([expYears + ' years', percentage]);
+        var percentage = (expYearsCount[expYears] / totalVacancies) * 100;
+        data.addRow([expYears + ' years', percentage]);
     });
 
     var options = {
-    chartArea: { width: '90%', height: '90%' },
-      legend: { position: 'right' },
-      pieSliceText: 'percentage',
-      pieStartAngle: 100,
-      slices: {
-        0: { color: '#3366cc' },
-        1: { color: '#dc3912' },
-        2: { color: '#ff9900' },
-        3: { color: '#109618' },
-        4: { color: '#990099' },
-        5: { color: '#0099c6' },
-        6: { color: '#dd4477' },
-        7: { color: '#66aa00' },
-        8: { color: '#b82e2e' },
-        9: { color: '#316395' }
-      }
+        chartArea: { width: '90%', height: '90%' },
+        legend: { position: 'right' },
+        pieSliceText: 'percentage',
+        pieStartAngle: 100,
+        slices: {
+            0: { color: '#3366cc' },
+            1: { color: '#dc3912' },
+            2: { color: '#ff9900' },
+            3: { color: '#109618' },
+            4: { color: '#990099' },
+            5: { color: '#0099c6' },
+            6: { color: '#dd4477' },
+            7: { color: '#66aa00' },
+            8: { color: '#b82e2e' },
+            9: { color: '#316395' }
+        }
     };
 
-     // Add the vacanciesStatus chart container
-     $('#vacancy-reports-doublecardwrapper').append(`<div class='report-wrapper'>
+    // Add the vacanciesStatus chart container
+    $('#vacancy-reports-doublecardwrapper').append(`<div class='report-wrapper'>
      <p class='reportTitle'> Years of Experience </p>
      <div id="vacanciesYOE"></div>
      </div>`);
- 
+
 
     var chart = new google.visualization.PieChart(document.getElementById('vacanciesYOE'));
     chart.draw(data, options);
-  }
+}
 
 function fetchVacancies() {
     return new Promise(function (resolve, reject) {
