57e184bcc599b7409dfa15648e3d30ed38d39f91
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-07-25 10:01:39 +0300

    Update and rename AnalyticsJS3.js to AnalyticsJS4.js

---
+++ AnalyticsJS4.js
@@ -0,0 +1,27 @@
+ // Function to draw the Google Chart
+    function drawChart() {
+      // Sample data for the chart
+      var data = google.visualization.arrayToDataTable([
+        ['Category', 'Value'],
+        ['Category 1', 10],
+        ['Category 2', 20],
+        ['Category 3', 30],
+        ['Category 4', 15],
+        ['Category 5', 25],
+      ]);
+
+      // Chart options
+      var options = {
+        title: 'Simple Bar Chart',
+        vAxis: { title: 'Values' },
+        hAxis: { title: 'Categories' },
+      };
+
+      // Instantiate and draw the chart
+      var chart = new google.visualization.ColumnChart(document.getElementById('chartContainer'));
+      chart.draw(data, options);
+    }
+
+    // Load the Google Charts API and draw the chart when the page loads
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
